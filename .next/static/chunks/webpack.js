d4e26fa7201b85624438c9da4692258ce92b8c7e
Charles-Abasimfon <devabascharles@gmail.com>
2022-12-27 14:22:50 +0100

    Fixes

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e463a486ae6405c2"; }
+/******/ 		__webpack_require__.h = function() { return "66eaaba575c4565c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
